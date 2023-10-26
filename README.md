8ff0013d95f96eb16d102fa37c9a669248c864b8
Jon Tsiros <jontsiros@scratchpad.com>
2023-10-26 13:03:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Assembly CLI
+
+The Assembly.ai CLI API Tester is a command-line tool written in Rust that allows you to submit video or audio files for transcription using the Assembly.ai API. It also provides the option to enable entity detection and IAB taxonomy categorization based on the generated transcript.
+
+## Features
+
+- Transcribe video or audio files with ease using the Assembly.ai API.
+- Entity detection and IAB taxonomy categorization.
+- Ability to submit files for transcription and retrieve the transcription results.
+
+## Prerequisites
+
+Before using the Assembly.ai CLI API Tester, ensure you have the following:
+
+- Rust installed on your system.
+- Assembly.ai API credentials (API key) which can be obtained from the [Assembly.ai website](https://assemblyai.com/).
+
+## Installation
+
+You can install the Assembly.ai CLI by building it from source. Here's how:
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/your-username/assembly-ai-cli.git
---
+++ src/main.rs
@@ -0,0 +1,106 @@
+use std::{env, process, thread::sleep, time::Duration};
+
+use dotenv::dotenv;
+use reqwest::{
+    blocking::Client,
+    header::{HeaderMap, HeaderValue},
+};
+use serde_json::{json, Value};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Args {
+    #[structopt(long = "recording-url")]
+    audio_url: Option<String>,
+
+    #[structopt(long = "transcript-id")]
+    transcript_id: Option<String>,
+}
+
+fn main() {
+    dotenv().ok();
+    // get audio_url argument to pass to assembly API
+    let args = Args::from_args();
+
+    let api_token = env::var("API_TOKEN").expect("API_TOKEN expected");
+    let transcript_url = env::var("TRANSCRIPT_URL").expect("TRANSCRIPT_URL expected");
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        reqwest::header::AUTHORIZATION,
+        HeaderValue::from_str(&api_token).expect("api_token as str"),
+    );
+    headers.insert(
+        reqwest::header::CONTENT_TYPE,
+        HeaderValue::from_static("application/json"),
+    );
+
+    let client = Client::new();
+
+    // transcript ID - either passed in as an arg, or
+    // we need to post recording, then get transcript to continue
+    let mut t_id: Option<String> = args.transcript_id;
+
+    if let Some(audio_url) = &args.audio_url {
+        let data = json!({
+            "audio_url": audio_url,
+            "iab_categories": true,
+            "entity_detection": true
+        });
+        let response = client
+            .post(&transcript_url)
+            .headers(headers.clone())
+            .json(&data)
+            .send()
+            .expect("err posting to transcript endpoint");
+
+        t_id = response
+            .json::<Value>()
+            .expect("could not read body of response")
+            .get("id")
+            .and_then(|v| v.as_str())
+            .map(String::from);
+    }
+    if let Some(transcript_id) = t_id {
+        poll_for_completion(&client, transcript_id, &headers, transcript_url);
+    } else {
+        eprintln!("no transcript id present for request");
+        process::exit(1);
+    }
+}
+
+fn poll_for_completion<S: AsRef<str>>(
+    client: &reqwest::blocking::Client,
+    transcript_id: S,
+    headers: &HeaderMap,
+    transcript_url: S,
+) {
+    let polling_endpoint = format!("{}/{}", transcript_url.as_ref(), transcript_id.as_ref());
+    loop {
+        let transcript_res = client
+            .get(&polling_endpoint)
+            .headers(headers.clone())
+            .send()
+            .expect("err get: transcript response");
+
+        let transcript_data: Value = transcript_res
+            .json()
+            .expect("could not read body of poll request");
+
+        let status = transcript_data.get("status").expect("status not present");
+        match status.as_str().expect("status as str") {
+            "completed" => {
+                println!("{}", transcript_data);
+                break;
+            }
+            "error" => {
+                eprintln!(
+                    "error response: {}",
+                    transcript_data.get("error").expect("error not present")
+                );
+                break;
+            }
+            _ => sleep(Duration::from_secs(10)),
+        };
+    }
+}
