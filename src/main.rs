0a24ef1fda23ac5e35b56bca275d43de19e14182
Jon Tsiros <jontsiros@scratchpad.com>
2023-11-13 10:51:20 -0600

    refactor/cleanup along with more descriptive errors if 'id' in response is not present

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use std::{env, process, thread::sleep, time::Duration};
+use std::{
+    env,
+    io::{self, ErrorKind},
+    process,
+    thread::sleep,
+    time::Duration,
+};
 
+use anyhow::Result;
 use dotenv::dotenv;
 use reqwest::{
     blocking::Client,
@@ -42,31 +49,81 @@ fn main() {
     let mut t_id: Option<String> = args.transcript_id;
 
     if let Some(audio_url) = &args.audio_url {
-        let data = json!({
-            "audio_url": audio_url,
-            "iab_categories": true,
-            "entity_detection": true
-        });
-        let response = client
-            .post(&transcript_url)
-            .headers(headers.clone())
-            .json(&data)
-            .send()
-            .expect("err posting to transcript endpoint");
-
-        t_id = response
-            .json::<Value>()
-            .expect("could not read body of response")
-            .get("id")
-            .and_then(|v| v.as_str())
-            .map(String::from);
+        t_id = match transcribe(&client, &headers, audio_url, &transcript_url) {
+            Ok(t_id) => Some(t_id),
+            Err(e) => {
+                eprintln!("ERROR: {e}");
+                None
+            }
+        };
     }
     if let Some(transcript_id) = t_id {
+        println!("waiting for transcription to finish...");
         poll_for_completion(&client, transcript_id, &headers, transcript_url);
     } else {
-        eprintln!("no transcript id present for request");
         process::exit(1);
     }
+}
+
+/// Sends an audio transcription request to a specified URL and retrieves the transcription ID from the response.
+///
+/// # Parameters
+/// - `client`: A reference to a `reqwest::blocking::Client` used to make the HTTP request.
+/// - `headers`: A reference to a `HeaderMap` containing HTTP headers for the request.
+/// - `audio_url`: A URL (as a string or a type that can be converted into a URL) pointing to the audio file to be transcribed.
+/// - `url`: The URL of the transcription service endpoint.
+///
+/// # Type Parameters
+/// - `S`: A generic type parameter constrained to types that can be referenced as a string (`AsRef<str>`),
+///        serialized with `serde`, and converted into a URL (`reqwest::IntoUrl`).
+///
+/// # Returns
+/// - `Result<String>`: On success, contains a `String` representing the transcription ID. On failure,
+///                     returns an error in line with `reqwest` and standard I/O error handling.
+///
+/// # Errors
+/// - Network or server-related errors encountered by `reqwest`.
+/// - JSON parsing errors when processing the response.
+/// - An error if the response body cannot be read or if the expected `id` key is not found in the JSON response.
+///   The specific error is an `io::Error` with `ErrorKind::NotFound` and a custom error message detailing
+///   the missing `id`.
+///
+fn transcribe<S>(
+    client: &reqwest::blocking::Client,
+    headers: &HeaderMap,
+    audio_url: S,
+    url: S,
+) -> Result<String>
+where
+    S: AsRef<str> + serde::ser::Serialize + reqwest::IntoUrl,
+{
+    let data = json!({
+        "audio_url": audio_url,
+        "iab_categories": true,
+        "entity_detection": true
+    });
+    let response = client
+        .post(url)
+        .headers(headers.clone())
+        .json(&data)
+        .send()
+        .expect("err posting to transcript endpoint");
+
+    let parsed_json = response.json::<Value>().map_err(|e| {
+        eprintln!("ERROR: could not read body of response: {}", e);
+        e
+    })?;
+
+    Ok(parsed_json
+        .get("id")
+        .and_then(|v| v.as_str())
+        .map(String::from)
+        .ok_or_else(|| {
+            io::Error::new(
+                ErrorKind::NotFound,
+                format!("'id' key not found in response body: {:?}", parsed_json),
+            )
+        })?)
 }
 
 fn poll_for_completion<S: AsRef<str>>(
@@ -75,7 +132,11 @@ fn poll_for_completion<S: AsRef<str>>(
     headers: &HeaderMap,
     transcript_url: S,
 ) {
-    let polling_endpoint = format!("{}/{}", transcript_url.as_ref(), transcript_id.as_ref());
+    let polling_endpoint = format!(
+        "{transcript_url}/{id}",
+        transcript_url = transcript_url.as_ref(),
+        id = transcript_id.as_ref()
+    );
     loop {
         let transcript_res = client
             .get(&polling_endpoint)
@@ -90,12 +151,12 @@ fn poll_for_completion<S: AsRef<str>>(
         let status = transcript_data.get("status").expect("status not present");
         match status.as_str().expect("status as str") {
             "completed" => {
-                println!("{}", transcript_data);
+                println!("{data}", data = transcript_data);
                 break;
             }
             "error" => {
                 eprintln!(
-                    "error response: {}",
+                    "ERROR: {}",
                     transcript_data.get("error").expect("error not present")
                 );
                 break;
